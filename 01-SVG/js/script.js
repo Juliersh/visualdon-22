090817a66ae3c14a8f40d63815770de5d5a039c2
Juliersh <julie.resch@heig-vd.ch>
2022-02-25 16:30:39 +0100

    exercice1

---
+++ 01-SVG/js/script.js
@@ -0,0 +1,27 @@
+
+
+domOn('rect', 'click', async evt => {
+    const btn = evt.currentTarget;
+    if (btn.style.fill === 'red') {
+        btn.style.fill = 'blue';   
+      }
+      else btn.style.fill = 'red';
+    }
+)
+
+domOn('.circle1', 'mousemove', async evt => {
+    const btn = evt.currentTarget;
+    if (btn.style.r === '60') {
+        btn.style.r = '100';   
+      }
+      else btn.style.r = '60';
+    }
+)
+
+function domForEach(selector, callback) {
+    document.querySelectorAll(selector).forEach(callback);
+}
+
+function domOn(selector, event, callback) {
+    domForEach(selector, ele => ele.addEventListener(event, callback));
+}
